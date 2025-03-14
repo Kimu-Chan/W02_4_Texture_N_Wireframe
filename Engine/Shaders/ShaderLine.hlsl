753c593d4be3b43d0258e6694e76b40de0f56a7f
Mstone8370 <mstone8370@gmail.com>
2025-03-14 14:03:07 +0900

    feat: prepare for world grid

---
+++ Engine/Shaders/ShaderLine.hlsl
@@ -0,0 +1,42 @@
+// ShaderW0.hlsl
+cbuffer constants : register(b0)
+{
+    matrix MVP;
+    float4 CustomColor;
+    uint bUseVertexColor;
+}
+
+struct VS_INPUT
+{
+    float4 position : POSITION; // Input position from vertex buffer
+    float4 color : COLOR;       // Input color from vertex buffer
+};
+
+struct PS_INPUT
+{
+    float4 position : SV_POSITION; // Transformed position to pass to the pixel shader
+    float4 color : COLOR;          // Color to pass to the pixel shader
+    // float4 depthPosition : TEXCOORD0;
+};
+
+struct PS_OUTPUT
+{
+    float4 color : SV_TARGET;
+    float depth : SV_Depth;
+};
+
+PS_INPUT mainVS(VS_INPUT input)
+{
+    PS_INPUT output;
+
+    output.position = mul(input.position, MVP);
+
+    output.color = bUseVertexColor == 1 ? input.color : CustomColor;
+    return output;
+}
+
+
+float4 mainPS(PS_INPUT input) : SV_TARGET
+{
+    return input.color;
+}
